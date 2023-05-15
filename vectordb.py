69025e5f53a145d22d6d0b7a4e34f63bbf332d4e
Aditya Vikram Thoomati <adivik2000@hotmail.com>
2023-05-15 20:10:42 +0530

    uncommitted changes

--- vectordb.py
+++ vectordb.py
@@ -1,15 +1,13 @@
 import math
 import os
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 import pinecone
 import weaviate
-import redis
 from datasets import load_dataset
 from loguru import logger
 from qdrant_client import QdrantClient
-from qdrant_client.http import models
 from qdrant_client.http.models import (
     CollectionStatus,
     Distance,
@@ -17,8 +15,6 @@ from qdrant_client.http.models import (
     UpdateStatus,
     VectorParams,
 )
-
-from qdrant_client.http.models import CollectionStatus, PointStruct, UpdateStatus
 from redis.commands.search.field import TextField, VectorField
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from redis.commands.search.query import Query
@@ -28,15 +24,18 @@ class VectorDatabase:
     """VectorDatabase class initializes the Vector Database index_name and loads the dataset
     for the usage of the subclasses."""
 
-    def __init__(self, index_name, top_k: int = 3):
+    def __init__(self, index_name, top_k: int = 3, select_range: Optional[int] = None):
         self.index_name = index_name
         logger.info(f"Index name: {self.index_name} initialized")
-        # Load the dataset
-        self.dataset = load_dataset(
-            "Cohere/wikipedia-22-12-simple-embeddings", split="train"
-        ).select(
-            range(1000)
-        )  # select a subset 1000 rows
+        if select_range:
+            self.dataset = load_dataset(
+                "Cohere/wikipedia-22-12-simple-embeddings", split="train"
+            ).select(range(select_range))
+        else:
+            # Load the full dataset
+            self.dataset = load_dataset(
+                "Cohere/wikipedia-22-12-simple-embeddings", split="train"
+            )
         logger.info(f"Dataset loaded with {len(self.dataset)} records")
         self.top_k = top_k
         self.dimension = 768
@@ -72,6 +71,7 @@ class PineconeDB(VectorDatabase):
         self.pinecone_index = pinecone.Index(index_name=index_name)
 
     def upsert(self) -> str:
+        logger.info(f"pinecone batch size: {self.batch_size}")
         logger.info(f"total vectors from upsert: {len(self.dataset)}")
         num_vectors = len(self.dataset)
         logger.info(f"total num of vectors from upsert: {num_vectors}")
@@ -93,12 +93,12 @@ class PineconeDB(VectorDatabase):
             ]
 
             logger.info(
-                f"Upserting batch {i + 1} of {num_batches}, from {start_idx} to {end_idx}"
+                f"Upserting batch {i + 1} of {num_batches}, from {start_idx} to {end_idx} to Pinecone"
             )
 
             self.pinecone_index.upsert(vectors_batch)
 
-        logger.info(f"Upserted {num_vectors} vectors")
+        logger.info(f"Upserted {num_vectors} vectors to Pinecone")
 
         return "Upserted successfully"
 
@@ -127,7 +127,8 @@ class PineconeDB(VectorDatabase):
 
     def delete_index(self) -> str:
         pinecone.delete_index(self.index_name)
-        return "Index deleted"
+        logger.info(f"Pinecone Index Deleted: {self.index_name}")
+        return "Pinecone Index deleted"
 
 
 class QdrantDB(VectorDatabase):
@@ -141,7 +142,7 @@ class QdrantDB(VectorDatabase):
 
     def __init__(self, index_name):
         super().__init__(index_name)
-        self.batch_size = 100  # Adjust the batch size as per your requirements
+        self.batch_size = 1000  # Adjust the batch size as per your requirements
 
         self.qdrant_client = QdrantClient(
             os.environ["QDRANT_URL"],
@@ -177,6 +178,7 @@ class QdrantDB(VectorDatabase):
         num_vectors = len(self.dataset)
         logger.info(f"total num of vectors from upsert: {num_vectors}")
         num_batches = math.ceil(num_vectors / self.batch_size)
+        logger.info(f"Qdrant batch size: {self.batch_size}")
 
         logger.info(f"Upserting {num_vectors} vectors in {num_batches} batches")
 
@@ -194,7 +196,7 @@ class QdrantDB(VectorDatabase):
             ]
 
             logger.info(
-                f"Upserting batch {i + 1} of {num_batches}, from {start_idx} to {end_idx}"
+                f"Upserting batch {i + 1} of {num_batches}, from {start_idx} to {end_idx} to Qdrant"
             )
 
             operation_info = self.qdrant_client.upsert(
@@ -204,7 +206,7 @@ class QdrantDB(VectorDatabase):
             if operation_info.status != UpdateStatus.COMPLETED:
                 raise Exception("Upsert failed")
 
-        logger.info(f"Upserted {num_vectors} vectors")
+        logger.info(f"Upserted {num_vectors} vectors to Qdrant")
 
         return "Qdrant Upserted successfully"
 
@@ -260,13 +262,14 @@ class QdrantDB(VectorDatabase):
 
     def delete_index(self) -> str:
         self.qdrant_client.delete_collection(collection_name=self.index_name)
+        logger.info(f"Qdrant Collection/Index deleted: {self.index_name}")
         return "Qdrant Collection/Index deleted"
 
 
 class WeaviateDB(VectorDatabase):
     def __init__(self, index_name):
         super().__init__(index_name)
-        self.batch_size = 50
+        self.batch_size = 1000
 
         self.weaviate_class = "WikipediaEmbeddings"
 
@@ -281,9 +284,9 @@ class WeaviateDB(VectorDatabase):
 
         schema = self.weaviate_client.schema.get()
         if any(d["class"] == self.weaviate_class for d in schema["classes"]):
-            logger.info("schema already exists")
+            logger.info(f"schema for {self.weaviate_class} already exists")
         else:
-            logger.info("schema does not exist, creating it")
+            logger.info(f"schema for {self.weaviate_class} does not exist, creating it")
             schema = {
                 "classes": [
                     {
@@ -303,7 +306,8 @@ class WeaviateDB(VectorDatabase):
             self.weaviate_client.schema.create(schema)
 
     def upsert(self) -> str:
-        self.weaviate_client.batch.configure(batch_size=100)
+        logger.info(f"Weaviate batch size: {self.batch_size}")
+        self.weaviate_client.batch.configure(batch_size=self.batch_size)
 
         with self.weaviate_client.batch as batch:
             for data in self.dataset:
@@ -316,7 +320,48 @@ class WeaviateDB(VectorDatabase):
                 )
 
         logger.info("All data added to weaviate")
-        return "Index deleted"
+
+        return "Upserted successfully"
+
+    def query(self, query_embedding: List[float]) -> dict:
+        # Weaviate Output:
+        # {
+        #     "result": {
+        #         "data": {
+        #             "Get": {
+        #                 "WikipediaEmbeddings": [
+        #                     {
+        #                         "_additional": {"certainty": 0.9381886720657349},
+        #                         "text": 'Tertullian was probably the first person to call these books the "Old Testament." He used the Latin name "vetus testamentum" in the 2nd century.',
+        #                     },
+        #                     {
+        #                         "_additional": {"certainty": 0.9381886720657349},
+        #                         "text": 'Tertullian was probably the first person to call these books the "Old Testament." He used the Latin name "vetus testamentum" in the 2nd century.',
+        #                     },
+        #                     {
+        #                         "_additional": {"certainty": 0.9374131262302399},
+        #                         "text": "Other themes in the Old Testament include salvation, redemption, divine judgment, obedience and disobedience, faith and faithfulness. Throughout there is a strong emphasis on ethics and ritual purity. God demands both.",
+        #                     },
+        #                 ]
+        #             }
+        #         }
+        #     }
+        # }
+        vec = {"vector": query_embedding}
+        result = (
+            self.weaviate_client.query.get(self.weaviate_class, ["text"])
+            .with_near_vector(vec)
+            .with_limit(self.top_k)
+            .with_additional(["certainty"])
+            .do()
+        )
+        # logger.info(f"weaviate result: {result}")
+        return result
+
+    def delete_index(self) -> str:
+        self.weaviate_client.schema.delete_class(self.weaviate_class)
+        logger.info(f"Weaviate Class/Index deleted: {self.weaviate_class}")
+        return "Weaviate Index/Class deleted"
 
 
 class RedisDB(VectorDatabase):
@@ -366,6 +411,7 @@ class RedisDB(VectorDatabase):
     def upsert(self) -> str:
         # Write data to redis
         pipeline = self.redis_client.pipeline(transaction=False)
+        redis_batch_count = 1
         for i, data in enumerate(self.dataset):
             # Use provided values by default or fallback
             key = data["id"]
@@ -384,50 +430,15 @@ class RedisDB(VectorDatabase):
             # Write batch
             if i % self.batch_size == 0:
                 pipeline.execute()
+                logger.info(f"Redis batch {redis_batch_count} written")
+                redis_batch_count += 1
 
         # Cleanup final batch
         pipeline.execute()
 
-        return "Upserted successfully"
+        return "Redis Upserted successfully"
 
     def query(self, query_embedding: List[float]) -> dict:
-        # Weaviate Output:
-        # {
-        #     "result": {
-        #         "data": {
-        #             "Get": {
-        #                 "WikipediaEmbeddings": [
-        #                     {
-        #                         "_additional": {"certainty": 0.9381886720657349},
-        #                         "text": 'Tertullian was probably the first person to call these books the "Old Testament." He used the Latin name "vetus testamentum" in the 2nd century.',
-        #                     },
-        #                     {
-        #                         "_additional": {"certainty": 0.9381886720657349},
-        #                         "text": 'Tertullian was probably the first person to call these books the "Old Testament." He used the Latin name "vetus testamentum" in the 2nd century.',
-        #                     },
-        #                     {
-        #                         "_additional": {"certainty": 0.9374131262302399},
-        #                         "text": "Other themes in the Old Testament include salvation, redemption, divine judgment, obedience and disobedience, faith and faithfulness. Throughout there is a strong emphasis on ethics and ritual purity. God demands both.",
-        #                     },
-        #                 ]
-        #             }
-        #         }
-        #     }
-        # }
-        vec = {"vector": query_embedding}
-        result = (
-            self.weaviate_client.query.get(self.weaviate_class, ["text"])
-            .with_near_vector(vec)
-            .with_limit(self.top_k)
-            .with_additional(["certainty"])
-            .do()
-        )
-        # logger.info(f"weaviate result: {result}")
-        return result
-
-    def delete_index(self) -> str:
-        self.weaviate_client.schema.delete_class(self.weaviate_class)
-        return "Class deleted"
         # base_query = (
         #     f"(@metadata:{ Old Testament })=>[KNN {self.top_k} @vector $query_embedding AS score]"
         # )
@@ -448,4 +459,5 @@ class RedisDB(VectorDatabase):
 
     def delete_index(self) -> str:
         self.redis_client.ft(self.index_name).drop_index(delete_documents=True)
-        return "Index deleted"
+        logger.info(f"Redis Index deleted: {self.index_name}")
+        return "Redis Index deleted"
